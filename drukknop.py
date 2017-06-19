b1fe4cdcdb0c7a462ca59ebc756059645db2a573
joachimbauters <joachim.bauters@student.howest.be>
2017-06-19 16:43:55 +0200

    commit

--- drukknop.py
+++ drukknop.py
@@ -4,8 +4,10 @@ import webbrowser
 
 knoparray = []
 drukknop = 21
+led=11
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
 GPIO.setup(drukknop, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 knop = GPIO.input(drukknop)
 i=1
@@ -15,15 +17,22 @@ while True:
         i+=1
         knoparray.append(i)
         time.sleep(0.5)
-        if i ==1:
+        if i == 0:
+            GPIO.output(led, GPIO.HIGH)
+        elif i ==1:
             webbrowser.open('http://localhost:8080', new=1)
+            GPIO.output(led, GPIO.HIGH)
         elif i ==2:
             webbrowser.open('http://localhost:8080/news', new=1)
+            GPIO.output(led, GPIO.HIGH)
         elif i == 3:
             webbrowser.open('http://localhost:8080/calendar', new=1)
+            GPIO.output(led, GPIO.HIGH)
         elif i == 4:
             webbrowser.open('http://localhost:8080/fotoslider', new=1)
+            GPIO.output(led, GPIO.HIGH)
         elif i == 5:
             del knoparray[:]
             i=0
             i+=1
+            GPIO.output(led, GPIO.LOW)
