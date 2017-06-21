a4aec8d9676a3577195baaf6500f2377f7be8f15
joachimbauters <joachim.bauters@student.howest.be>
2017-06-21 15:28:14 +0200

    commit

--- dbconn.py
+++ dbconn.py
@@ -21,13 +21,20 @@ class DbClass:
         self.__cursor.close()
         return result
 
+    def getimagesfromdatabase(self):
+        sqlquery = "SELECT Imagefile FROM ENMDatabase.Images"
+        self.__cursor.execute(sqlquery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
 
-    # def setDataToDatabase(self, value1):
-    #     # Query met parameters
-    #     sqlQuery = "INSERT INTO tablename (columnname) VALUES ('{param1}')"
-    #     # Combineren van de query en parameter
-    #     sqlCommand = sqlQuery.format(param1=value1)
-    #
-    #     self.__cursor.execute(sqlCommand)
-    #     self.__connection.commit()
-    #     self.__cursor.close()
+
+    def setDataToDatabase(self, value1):
+        # Query met parameters
+        sqlQuery = "INSERT INTO Temperatuursmeting (SensorWaarde) VALUES ('{param1}')"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=value1)
+
+        self.__cursor.execute(sqlCommand)
+        self.__connection.commit()
+        self.__cursor.close()
